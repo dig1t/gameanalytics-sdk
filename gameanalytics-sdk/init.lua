e2ede240bf34f5ce4b7d76aef4086baae57e8f37
Javier Mejia <4343867+dig1t@users.noreply.github.com>
2024-07-01 19:40:06 -0500

    export types

--- gameanalytics-sdk/init.lua
+++ gameanalytics-sdk/init.lua
@@ -1,5 +1,19 @@
 local RunService = game:GetService("RunService")
 
+local Types = require(script.GameAnalytics.Types)
+
+export type BusinessEventOptions = Types.BusinessEventOptions
+export type ResourceEventOptions = Types.ResourceEventOptions
+export type ProgressionEventOptions = Types.ProgressionEventOptions
+export type DesignEventOptions = Types.DesignEventOptions
+export type ErrorEventOptions = Types.ErrorEventOptions
+export type CustomDimension = Types.CustomDimension
+export type ProductInfo = Types.ProductInfo
+export type ProcessReceiptInfo = Types.ProcessReceiptInfo
+export type TeleportData = Types.TeleportData
+export type RemoteConfigs = Types.RemoteConfigs
+export type GameAnalyticsOptions = Types.GameAnalyticsOptions
+
 --[[
 	This script determines if we should load gameanalytics server or client.
 ]]
